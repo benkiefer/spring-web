c133e92250d6200a0c6b5eb59761b0804017d8af
Ben Kiefer <kieferfam@msn.com>
2012-06-03 21:16:44 -0500

    Ben: finishing up integration tests for movie list page

---
+++ integration/src/test/groovy/org/burgers/spring/web/integration/ListMoviesTest.groovy
@@ -0,0 +1,65 @@
+package org.burgers.spring.web.integration
+
+import com.gargoylesoftware.htmlunit.WebClient
+import com.gargoylesoftware.htmlunit.html.HtmlPage
+import org.burgers.spring.web.domain.Rating
+import org.burgers.spring.web.domain.Repository
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+
+import static org.burgers.spring.web.integration.util.IntegrationConstants.*
+import static org.burgers.spring.web.integration.util.IntegrationUtils.cleanDatabase
+import org.burgers.spring.web.domain.Movie
+import com.gargoylesoftware.htmlunit.html.HtmlTable
+
+@RunWith(SpringJUnit4ClassRunner)
+@ContextConfiguration(locations=["classpath*:/contexts/DatabaseContext.xml"])
+class ListMoviesTest {
+    @Autowired Repository repository
+
+    WebClient webClient
+
+    @Before
+    void setUp(){
+        webClient = new WebClient()
+        cleanDatabase(repository)
+    }
+
+    @Test
+    void happyPath(){
+        repository.save(new Movie(title: "Jaws", rating: Rating.G))
+        repository.save(new Movie(title: "Sesame Street", rating: Rating.R))
+
+        HtmlPage page = webClient.getPage(HOME_URL + "list.do")
+        assert page.titleText == "Movie List"
+
+        HtmlTable table = (HtmlTable) page.getElementById("movies")
+
+        assertRow 2, "Jaws", "G", table
+        assertRow 3, "Sesame Street", "R", table
+    }
+
+    @Test
+    void happyPath_no_movies(){
+        HtmlPage page = webClient.getPage(HOME_URL + "list.do")
+        assert page.titleText == "Movie List"
+
+        assert page.body.textContent.contains("There are currently no movies. Go add one.")
+    }
+
+    private void assertRow(int index, String title, String rating, HtmlTable table){
+        table.getRow(index).cells[0].textContent == title
+        table.getRow(index).cells[1].textContent == rating
+    }
+
+    @After
+    void tearDown(){
+        webClient.closeAllWindows()
+    }
+
+}
--- mvc-example/src/main/webapp/WEB-INF/jsp/movieList.jsp
+++ mvc-example/src/main/webapp/WEB-INF/jsp/movieList.jsp
@@ -1,4 +1,5 @@
 <%@ taglib uri="http://java.sun.com/jstl/core_rt" prefix="c" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
 
 <head>
     <title>Movie List</title>
@@ -7,21 +8,33 @@
 <body>
     <div class="contentArea">
 	    <h1>Movie List</h1>
-	    <p>Here are the current movies:</p>
 
-        <table class="dataTable">
-                <tr>
-                    <td class="dataTableColumnHeading">Title:</td>
-                    <td class="dataTableColumnHeading">Rating:</td>
-                <tr>
+	    <c:choose>
 
-	            <c:forEach var="movie" items="${movies}" varStatus="status">
-	                    <tr class="dataTableRow">
-	                        <td class="dataTableText">${movie.title}</td>
-	                        <td class="dataTableText">${movie.rating}</td>
-                        </tr>
-                </c:forEach>
-        </table>
+            <c:when test="${fn:length(movies) gt 0}">
+
+                <p>Here are the current movies:</p>
+
+                <table id="movies" class="dataTable">
+                        <tr>
+                            <td class="dataTableColumnHeading">Title:</td>
+                            <td class="dataTableColumnHeading">Rating:</td>
+                        <tr>
+                        <c:forEach var="movie" items="${movies}" varStatus="status">
+                            <tr class="dataTableRow">
+                                <td class="dataTableText">${movie.title}</td>
+                                <td class="dataTableText">${movie.rating}</td>
+                            </tr>
+                        </c:forEach>
+                </table>
+
+            </c:when>
+
+            <c:otherwise>
+                <p>There are currently no movies. Go add one.</p>
+            </c:otherwise>
+
+	    </c:choose>
 
     </div>
 </body>
